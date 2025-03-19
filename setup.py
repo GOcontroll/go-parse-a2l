ee6a259ba1ec2cbf05d381f36f039c9093ffb15b
Maud Spierings <maud_spierings@hotmail.com>
2025-03-19 13:33:22 +0100

    add debian packaging

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (root / "README.md").read_text()
 setup(
     name = "go-parse-a2l",
     version = "1.0.0",
-    description = "A script to parse an a2l file and turn them into json",
+    description = "A script to parse an a2l file and turn it into json",
     url="https://github.com/GOcontroll/go-parse-a2l",
     license="MIT",
     long_description=long_description,
@@ -18,10 +18,12 @@ setup(
     author="GOcontroll",
     author_email="info@gocontroll.com",
     maintainer="Maud Spierings",
-    packages=["go-parse-a2l"],
+    install_requires=["pyuio"],
+    packages=["go_parse_a2l"],
     entry_points={
         "console_scripts": [
-            "go-parse-a2l = go-parse-a2l.parse_a2l:parse_a2l",
+            "go-parse-a2l = go_parse_a2l.parse_a2l:parse_a2l",
         ]
-    }
+    },
+    python_requires=">=3.9",
 )
